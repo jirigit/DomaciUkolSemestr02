d07c849b5806e4c3c3df4bfd8505e168f8c293f8
Jiri Kodys <>
2023-11-26 07:04:18 +0100

    uprava fake data

--- uu_homework_maing01-hi/src/fakeData/fakeData.js
+++ uu_homework_maing01-hi/src/fakeData/fakeData.js
@@ -1,19 +1,19 @@
 const users = [
   {
     id: "9586-6585-4581-0000",
-    name: "Petr Koller Owner",
+    name: "Jiri Kodys Owner",
   },
   {
     id: "9586-6585-4581-0001",
-    name: "Jan Novak Member",
+    name: "Misa Dvorakova Member",
   },
   {
-    id: "9586-6585-4581-0002",
-    name: "Petr Novak Member",
+    id: "9586-6585-4581-002",
+    name: "Maty Kodys Member",
   },
   {
     id: "9586-6585-4581-0003",
-    name: "Jan Koller Other",
+    name: "Klara Kodysova Other",
   },
 ];
 
